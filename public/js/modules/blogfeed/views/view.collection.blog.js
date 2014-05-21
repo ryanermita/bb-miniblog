4ad0e8198a20d00c386567475683391fd634e32b
Ryan Jayson Ermita <ryan.ermita@infoshiftinc.com>
2014-05-22 00:16:11 +0800

    add dummy data, and modified adding of new model.

--- public/js/modules/blogfeed/views/view.collection.blog.js
+++ public/js/modules/blogfeed/views/view.collection.blog.js
@@ -11,51 +11,141 @@ function(
 
    //define collection as blogs.
    //define model as posts.
-   blogs, posts
+   blogs,
+   posts
 ){
-  var blogData = [];
-
-  var BlogFeed = Backbone.View.extend({
-    //wrap models in 'ul' tag.
-    tagName: 'ul',
-    // define to use existing element on the page.
-    el: 'body',
-    initialize: function(){
-      //bind collection on add event,
-      //so whenener a new record added render will follow.
-      this.collection.on('add', this.render, this);
+   var blogData = [
+    {
+      title: 'The Amazing BackboneJS',
+      content: ['Lorem ipsum dolor sit amet, usus laoreet', 
+      'quis congue videntur nostrud. Erat elit', 
+      'non vel esse legentis parum per sequitur.', 
+      'Cum, me assum ullamcorper eu nibh velit. ',
+      'Ut blandit, euismod in, et tincidunt quod,',  
+      'at. Iriure ad fiant ipsum est litterarum typi',  
+      'sit consuetudium clari saepius. Sollemnes, eodem ', 
+      'claritas ipsum feugiat legere id nunc, iusto. ',
+      'Sequitur eua aliquam lobortis volutpat vulputate',  
+      'te, nulla veniam. Augue typi adipiscing claritatem',  
+      'lectorum insitam. Anteposuerit illum ii ex odio ',
+      'lorem te nostrud enim. Seacula me nunc congue.',
+      'Autem diam nulla quarta est. Vero nibh placerat non hendrerit.',
+      'ssim typi, facilisi, veniam, eum, augue facit,', 
+      'decima at ad, dolor. Molestie cum, duis wisi futurum ',
+      'videntur consequat quis eua. Legunt et eu quod. Nostrud', 
+      'sollemnes nunc, illum id claritatem. Clari, commodo quarta i',
+      'congue, etiam modo ii mirum in, iusto eum.',
+      'Anteposuerit seacula ex, te, sit me quarta quod, per est', 
+      'assum autem. Vero luptatum eodem non nulla notare erat nam', 
+      'congue accumsan ipsum. At mazim ad veniam vel velit tincidunt', 
+      'diam. Cum eua et saepius tempor legere legentis eleifend eu',
+      'esse, vero. Sollemnes odio, id nostrud ii usus legunt in', 
+      'possim. Ex seacula exerci te liber me est magna luptatum', 
+      'euismod consuetudium.'],
+      author: 'John Doe'
     },
-    render: function(){
-      //iterate collection to render in our view
-      this.collection.each(function(post){
-        var elPosts = new posts({model: post}).render().el;
-        this.$el.append(elPosts); 
-      }, this);
-      return this;
+    {
+      title: 'Ryan Learning BackboneJS',
+      content: ['Lorem ipsum dolor sit amet, usus laoreet', 
+      'quis congue videntur nostrud. Erat elit', 
+      'non vel esse legentis parum per sequitur.', 
+      'Cum, me assum ullamcorper eu nibh velit. ',
+      'Ut blandit, euismod in, et tincidunt quod,',  
+      'at. Iriure ad fiant ipsum est litterarum typi',  
+      'sit consuetudium clari saepius. Sollemnes, eodem ', 
+      'claritas ipsum feugiat legere id nunc, iusto. ',
+      'Sequitur eua aliquam lobortis volutpat vulputate',  
+      'te, nulla veniam. Augue typi adipiscing claritatem',  
+      'lectorum insitam. Anteposuerit illum ii ex odio ',
+      'lorem te nostrud enim. Seacula me nunc congue.',
+      'Autem diam nulla quarta est. Vero nibh placerat non hendrerit.',
+      'ssim typi, facilisi, veniam, eum, augue facit,', 
+      'decima at ad, dolor. Molestie cum, duis wisi futurum ',
+      'videntur consequat quis eua. Legunt et eu quod. Nostrud', 
+      'sollemnes nunc, illum id claritatem. Clari, commodo quarta i',
+      'congue, etiam modo ii mirum in, iusto eum.',
+      'Anteposuerit seacula ex, te, sit me quarta quod, per est', 
+      'assum autem. Vero luptatum eodem non nulla notare erat nam', 
+      'congue accumsan ipsum. At mazim ad veniam vel velit tincidunt', 
+      'diam. Cum eua et saepius tempor legere legentis eleifend eu',
+      'esse, vero. Sollemnes odio, id nostrud ii usus legunt in', 
+      'possim. Ex seacula exerci te liber me est magna luptatum', 
+      'euismod consuetudium.'],
+      author: 'Ryan Jayson Ermita'
     },
-    events:{
-      //event element: function
-      'click #addPost': 'addPost',
-      'click #showAddPost': 'showAddPost'
+    {
+      title: 'Awesome BackboneJS!',
+      content: ['Lorem ipsum dolor sit amet, usus laoreet', 
+      'quis congue videntur nostrud. Erat elit', 
+      'non vel esse legentis parum per sequitur.', 
+      'Cum, me assum ullamcorper eu nibh velit. ',
+      'Ut blandit, euismod in, et tincidunt quod,',  
+      'at. Iriure ad fiant ipsum est litterarum typi',  
+      'sit consuetudium clari saepius. Sollemnes, eodem ', 
+      'claritas ipsum feugiat legere id nunc, iusto. ',
+      'Sequitur eua aliquam lobortis volutpat vulputate',  
+      'te, nulla veniam. Augue typi adipiscing claritatem',  
+      'lectorum insitam. Anteposuerit illum ii ex odio ',
+      'lorem te nostrud enim. Seacula me nunc congue.',
+      'Autem diam nulla quarta est. Vero nibh placerat non hendrerit.',
+      'ssim typi, facilisi, veniam, eum, augue facit,', 
+      'decima at ad, dolor. Molestie cum, duis wisi futurum ',
+      'videntur consequat quis eua. Legunt et eu quod. Nostrud', 
+      'sollemnes nunc, illum id claritatem. Clari, commodo quarta i',
+      'congue, etiam modo ii mirum in, iusto eum.',
+      'Anteposuerit seacula ex, te, sit me quarta quod, per est', 
+      'assum autem. Vero luptatum eodem non nulla notare erat nam', 
+      'congue accumsan ipsum. At mazim ad veniam vel velit tincidunt', 
+      'diam. Cum eua et saepius tempor legere legentis eleifend eu',
+      'esse, vero. Sollemnes odio, id nostrud ii usus legunt in', 
+      'possim. Ex seacula exerci te liber me est magna luptatum', 
+      'euismod consuetudium.'],
+      author: 'Ryan Ermita'
     },
-    addPost: function(){
-      console.log('add me');
-      var title = this.$('#addTitle').val(),
-          author = this.$('#addAuthor').val(),
-          content = this.$('#addContent').val();
-      this.collection.add({'title': title, 'author': author, 'content': content});
-      this.$('#addPostForm').attr('hidden', 'hidden');
-      this.$('.blogfeed').removeAttr('hidden');
-      console.log(title + author + content);
-    },
-    showAddPost: function(){
-      console.log('show add form');
-      this.$('#addTitle').val(''),
-      this.$('#addAuthor').val(''),
-      this.$('#addContent').val('');
-      this.$('#addPostForm').removeAttr('hidden');
-      this.$('.blogfeed').attr('hidden', 'hidden');
-    }
+  ]; 
+   var BlogFeed = Backbone.View.extend({ //wrap models in 'ul' tag.
+     tagName: 'ul',
+     // define to use existing element on the page.
+     el: 'body',
+     initialize: function(){
+       //bind collection on add event,
+       //so whenener a new record added render will follow.
+       this.collection.on('add', this.addNewPost, this);
+     },
+     render: function(){
+       //iterate collection to render in our view
+       this.collection.each(function(post){
+         this.addNewPost(post);
+       }, this);
+       return this;
+     },
+     addNewPost: function(post){
+       var elPosts = new posts({model: post}).render().el;
+       this.$el.append(elPosts); 
+     },
+     events:{
+       //event element: function
+       'click #addPost': 'addPost',
+       'click #showAddPost': 'showAddPost'
+     },
+     addPost: function(){
+       console.log('add me');
+       var title = this.$('#addTitle').val(),
+           author = this.$('#addAuthor').val(),
+           content = this.$('#addContent').val();
+       this.collection.add({'title': title, 'author': author, 'content': content});
+       this.$('#addPostForm').attr('hidden', 'hidden');
+       this.$('.blogfeed').removeAttr('hidden');
+       console.log(title + author + content);
+     },
+     showAddPost: function(){
+       console.log('show add form');
+       this.$('#addTitle').val(''),
+       this.$('#addAuthor').val(''),
+       this.$('#addContent').val('');
+       this.$('#addPostForm').removeAttr('hidden');
+       this.$('.blogfeed').attr('hidden', 'hidden');
+     }
   });
  return new BlogFeed({collection: new blogs(blogData)});
 });
